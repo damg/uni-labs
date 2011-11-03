43cce83a54b24f25ceb46cf0ba79360beb70b5fa
Dmitri Bachtin <d.bachtin@gmail.com>
2011-11-04 00:28:40 +0100

    Digram exercise by lookahead.

---
+++ compiler_construction/lexers/common.mk
@@ -0,0 +1,22 @@
+CC=gcc
+CFLAGS=-Wall
+LDFLAGS=
+LEX=flex
+
+all: compile
+
+compile: $(TARGET)
+
+$(TARGET): $(TARGET).yy.o
+	$(CC) $(LDFLAGS) -o $@ $<
+
+%.yy.o: %.yy.c
+	$(CC) $(CFLAGS) -c $<
+
+%.yy.c: %.l
+	$(LEX) -o $@ $<
+
+clean:
+	$(RM) -fv *.o *.yy.c *~ $(TARGET)
+
+.PHONY: all compile clean
