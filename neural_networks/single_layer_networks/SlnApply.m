4902a4f78d4110d426d3cb6af351cb08f565062d
unknown <s.hartlieb@nahum.(none)>
2011-11-09 14:19:26 +0100

    Matlabified sources

--- neural_networks/single_layer_networks/SlnApply.m
+++ neural_networks/single_layer_networks/SlnApply.m
@@ -1,25 +1,25 @@
-## -*- mode: octave -*-
+%% -*- mode: octave -*-
 
-## Copyright (C) 2011 Dmitri Bachtin
-## 
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with Octave; see the file COPYING.  If not, see
-## <http://www.gnu.org/licenses/>.
+%% Copyright (C) 2011 Dmitri Bachtin, Sven Hartlieb
+%% 
+%% This program is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation; either version 2 of the License, or
+%% (at your option) any later version.
+%% 
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%% 
+%% You should have received a copy of the GNU General Public License
+%% along with Octave; see the file COPYING.  If not, see
+%% <http://www.gnu.org/licenses/>.
 
-## SlnApply
+%% SlnApply
 
-## Author: Dmitri Bachtin <dima2001@MS-7673>
-## Created: 2011-11-01
+%% Author: Dmitri Bachtin <dima2001@MS-7673>
+%% Created: 2011-11-01
 
 function [ ret ] = SlnApply (net, xs)
   [h, w] = size(xs);
@@ -28,4 +28,4 @@ function [ ret ] = SlnApply (net, xs)
   end
   y = net.W1*xs' + net.b1;
   ret = net.actFn(y);
-endfunction
+end
