4ca0a7e97a27347c60a17305e6c0284516035acb
Dmitri Bachtin <d.bachtin@gmail.com>
2011-11-30 12:15:46 +0100

    ppr_mq implemented

--- parallel_processes/ppr_mq.c
+++ parallel_processes/ppr_mq.c
@@ -1,28 +1,67 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
 #include <semaphore.h>
+#include <signal.h>
+#include <libgen.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+void atexit_handler(void);
 
 void start_conv(void);
 void start_log(void);
 void start_statistics(void);
 void start_report(void);
 
+void report_sighandler(int sig);
+void log_sighandler(int sig);
+
+static int pipe_conv_to_log;
+static int pipe_conv_to_statistics;
+static int pipe_statistics_to_report;
+
+extern int errno;
+
+struct conv_msgbuf
+{
+  long mtype;
+  int mnum;
+};
+
+struct stat_msgbuf
+{
+  long mtype;
+  int mmin_num;
+  int mmax_num;
+  double mmean_num;
+};
+
+static char *app_name;
+
 int
 main(int argc, char *argv[])
 {
+  app_name = strdup(basename(argv[0]));
+  atexit(atexit_handler);
+
   pid_t pid;
 
   srand(time(0));
 
-  pid = fork();
-  if (pid)
-    start_log();
+  pipe_conv_to_log = msgget(IPC_PRIVATE, IPC_CREAT | 0777);
+  pipe_conv_to_statistics = msgget(IPC_PRIVATE, IPC_CREAT | 0777);
+  pipe_statistics_to_report = msgget(IPC_PRIVATE, IPC_CREAT | 0777);
 
   pid = fork();
-  if (pid)
-    start_statistics();
+  if (!pid)
+      start_log();
+
+  pid = fork();
+  if (!pid)
+      start_statistics();
 
   start_conv();
   return EXIT_SUCCESS;
@@ -31,38 +70,140 @@ main(int argc, char *argv[])
 void start_conv(void)
 {
   int num;
+  struct conv_msgbuf buf;
 
   while(1)
     {
-      num = rand() % sizeof(unsigned char);
-      usleep(300000);
+      usleep(1000000);
+      num = rand() % 256;
+
+      buf.mtype = 1;
+      buf.mnum = num;
+
+      msgsnd(pipe_conv_to_log, &buf, sizeof(buf.mnum), 0);
+      msgsnd(pipe_conv_to_statistics, &buf, sizeof(buf.mnum), 0);
     }
 }
 
 void start_log(void)
 {
+  char *log_file_path;
+  static FILE *log_f;
+  int num;
+  size_t log_file_path_len;
+  struct conv_msgbuf buf;
+
+  log_file_path_len = strlen(app_name) + 9 + 1;
+  log_file_path = malloc(log_file_path_len);
+  memset(log_file_path, 0, log_file_path_len);
+  sprintf(log_file_path, "/tmp/%s.log", app_name);
+  fprintf(stderr, "Log file: %s\n", log_file_path);
+  log_f = fopen(log_file_path, "w");
+  free(log_file_path);
+
+  signal(SIGINT, log_sighandler);
+
   while(1)
     {
+      msgrcv(pipe_conv_to_log, &buf, sizeof(buf.mnum), 1, 0);
+      num = buf.mnum;
+      fprintf(log_f, "[%ld] %d\n", time(0), num);
+      fflush(log_f);
     }
 }
 
 void start_report(void)
 {
+  size_t report_file_path_len;
+  char *report_file_path;
+  static FILE *report_f;
+
+  int min_num, max_num, num;
+  double mean_num;
+  struct stat_msgbuf buf;
+
+  report_file_path_len = strlen(app_name) + 9 + 1;
+  report_file_path = malloc(report_file_path_len);
+  memset(report_file_path, 0, report_file_path_len);
+  sprintf(report_file_path, "/tmp/%s.rep", app_name);
+  fprintf(stderr, "Report file: %s\n", report_file_path);
+  report_f = fopen(report_file_path, "w");
+  free(report_file_path);
+
+  signal(SIGINT, report_sighandler);
+
   while(1)
     {
+      msgrcv(pipe_statistics_to_report, &buf, sizeof(buf)-sizeof(long), 2, 0);
+      min_num = buf.mmin_num;
+      max_num = buf.mmax_num;
+      mean_num = buf.mmean_num;
+
+      fprintf(report_f, "[%ld] %i %i %f\n", time(0), min_num, max_num, mean_num);
+      fflush(report_f);
     }
 }
 
 void start_statistics(void)
 {
   pid_t pid;
+  int num;
+
+  int min_num, max_num;
+  size_t count;
+  int sum;
+  double mean_num;
+
+  struct conv_msgbuf rcvbuf;
+  struct stat_msgbuf sndbuf;
+
+  count = 0;
+  min_num = max_num = 0;
+  sum = 0;
+  mean_num = 0.0;
 
   pid = fork();
-  if (pid)
-    start_report();
+  if (!pid)
+      start_report();
 
   while(1)
     {
+      msgrcv(pipe_conv_to_statistics, &rcvbuf, sizeof(int), 0, 0);
+      num = rcvbuf.mnum;
+
+      ++count;
+      sum += num;
+      if (num < min_num)
+	min_num = num;
+      if (num > max_num)
+	max_num = num;
+      mean_num = ((double) sum) / ((double) count);
+
+      sndbuf.mtype = 2;
+      sndbuf.mmin_num = min_num;
+      sndbuf.mmax_num = max_num;
+      sndbuf.mmean_num = mean_num;
+      msgsnd(pipe_statistics_to_report, &sndbuf, sizeof(sndbuf)-sizeof(long), 0);
     }
 }
 
+void log_sighandler(int sig)
+{
+  static FILE *log_f;
+  fclose(log_f);
+}
+
+void report_sighandler(int sig)
+{
+  static FILE *report_f;
+  fclose(report_f);
+}
+
+void atexit_handler(void)
+{
+  msgctl(pipe_conv_to_log, IPC_RMID, NULL);
+  msgctl(pipe_conv_to_statistics, IPC_RMID, NULL);
+  msgctl(pipe_statistics_to_report, IPC_RMID, NULL);
+
+  free(app_name);
+}
